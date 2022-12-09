b7f4baf8ad9b080131e480496e511c19b6dc2f9c
zacjjohnson <wecooked@gmail.com>
2022-12-09 11:18:01 -0500

    working on layout and info recieved from dropbox to display player and music on beats and homepage

--- client/src/components/beats/beats.component.jsx
+++ client/src/components/beats/beats.component.jsx
@@ -9,20 +9,22 @@ import { useState } from 'react';
 
     // const [ beatsFromDropbox, setBeatsFromDropbox ] = useState(null);
 
-       let beatsFromDropbox = [];
+      //  let beatsFromDropbox = [];
 
     const { open, isOpen } = useDropboxChooser({
       appKey: 'voxbiyjxw9yt7tk',
       chooserOptions: { multiple: true, linkType: 'direct', multiselect: true },
       onSelected: files => {
-        beatsFromDropbox.push(files);
-        
+        // beatsFromDropbox.push(files);
+        console.log(files)
         
       },
     });
 
    
-
+    // const beatsFromDropbox = () => {
+      
+    // }
     
     
 
@@ -31,17 +33,25 @@ import { useState } from 'react';
       <>
       <div className="display-beats">
         
-        
+        <div className='beats-info-boxes'>
+          <div className='beat-box'>
 
+          <h3>Name of Beat Here</h3>
+          <audio 
+            controls 
+            src='https://dl.dropboxusercontent.com/1/view/z2pq2cg3l5hf0hw/Artists/ZAYH/NEW%20BEATS/WRITA%20BEATS/%20.wav'></audio>
+          <button className='add-to-cart'>Add to Cart</button>
+          </div>
+        </div>
       </div>
-      <div className="upload-beats">
-
-        <button className="dropbox-button" onClick={open} disabled={isOpen}>
-          <img className="dropbox-logo" src={dropboxLogo} alt="dropbox logo" />
-          Choose from Dropbox
-        </button>
-
-      </div>
+      
+        <div className='button-container'>
+          <button className="dropbox-button" onClick={open} disabled={isOpen}>
+            <img className="dropbox-logo" src={dropboxLogo} alt="dropbox logo" />
+            Choose from Dropbox
+          </button>
+        </div>
+      
       </>
     )
   }
